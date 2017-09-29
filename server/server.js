ea004cf5bafee7e82d43f72b912a052a0cde18a6
Himanshu Arora <himanshuarora9419@gmail.com>
2017-09-29 19:27:20 +0530

    socket io seed

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const path=require('path');
+const http=require('http');
 const publicPath=path.join(__dirname,'/..','/public');
 const express=require('express');
-console.log(__dirname+'/../public');
-console.log(publicPath);
+const socketIO=require('socket.io');
 const port=process.env.PORT || 3000;
 
 var app=express();
-
+var server=http.createServer(app);
 app.use(express.static(publicPath));
+var io=socketIO(server);
+io.on('connection',(socket)=>{
+    console.log("new user connected");
+    socket.on('disconnect',() => {
+        console.log('client disconnected');
+    })
+});
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('server is up on port 3000');
 })
