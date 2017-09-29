c1d3539e5c56bb0a00723a0e777d1ea2d5155d49
Himanshu Arora <himanshuarora9419@gmail.com>
2017-09-29 22:31:51 +0530

    message function

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const publicPath=path.join(__dirname,'/..','/public');
 const express=require('express');
 const socketIO=require('socket.io');
 const port=process.env.PORT || 3000;
+const {generateMessage}=require('./utils/message.js');
 
 var app=express();
 var server=http.createServer(app);
@@ -11,29 +12,10 @@ app.use(express.static(publicPath));
 var io=socketIO(server);
 io.on('connection',(socket)=>{
     console.log("new user connected");
-    socket.emit('newMessage',{
-        "from":"Admin",
-        "text":"Welcome to chat",
-        "createdAt":new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage',{
-        "from":"Admin",
-        "text":"new user joined",
-        "createdAt":new Date().getTime()
-    })
-    socket.on('disconnect',() => {
-        console.log('client disconnected');
-    });
-    // socket.emit('newEmail',{
-    //     "from":"Mike",
-    //     "text":"whats going on",
-    //     "createAt":"123"
-    // });
-    socket.emit('newMessage',{
-        "from":"Mike",
-        "text":"whats going on",
-        "createAt":"123"
-    });
+    socket.emit('newMessage',generateMessage("Admin","Welcome to chat"));
+    socket.broadcast.emit('newMessage',generateMessage("Admin","new user joined"));
+    socket.on('disconnect',() => {console.log('client disconnected');});
+    socket.emit('newMessage',generateMessage("Mike","whats going on"));
     socket.on('createMessage',(message)=>{
         console.log("msg getting from user", message);
         // io.emit('newMessage',{
@@ -41,11 +23,7 @@ io.on('connection',(socket)=>{
         //     text:message.text,
         //     createdAt: new Date().getTime()
         // })
-        socket.broadcast.emit('newMessage',{
-                 from:message.from,
-                 text:message.text,
-                 createdAt: new Date().getTime()
-        });
+        socket.broadcast.emit('newMessage',generateMessage(message.from,message.text));
     })
     // socket.on('createEmail',(data)=>{
     //     console.log("mail getting from user");
