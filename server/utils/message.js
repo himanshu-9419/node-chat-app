534c5893df302d1913c5c4905e19cdb4cf006838
Himanshu Arora <himanshuarora9419@gmail.com>
2017-09-30 20:41:28 +0530

    adding moment

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,10 +1,11 @@
+var moment=require('moment');
 var generateMessage=function(from,text){
-    return {"from":from,"text":text,"createdAt":new Date().getTime()}
+    return {"from":from,"text":text,"createdAt":moment().valueOf()}
 }
 var generateLocationMessage=function(from,latitude,longitude){
     return {
         "from":from,
         "url":`https://www.google.com/maps?q=${latitude},${longitude}`,
-        "createdAt":new Date().getTime()}
+        "createdAt":moment().valueOf()}
 }
 module.exports = {generateMessage,generateLocationMessage};
